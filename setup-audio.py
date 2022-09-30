115dadd439c7174971540776128977ad65caa614
WeirdTreeThing <bradyn127@protonmail.com>
2022-09-29 22:30:16 -0400

    Fix sof

--- setup-audio.py
+++ setup-audio.py
@@ -5,18 +5,39 @@ import subprocess
 import os
 import json
 
+def cpfile(srcf, dstf):
+    src = Path(srcf)
+    dst = Path(dstf)
+    dst.write_bytes(src.read_bytes())
+
 def sklkbl_audio():
     print("sklkbl audio not implemented yet")
 
 def apl_audio():
     print("aplaudio not implemented yet")
 
-def sof_audio():
+def sof_audio(cpu, username):
+    # Install required packages
     install_package("sof-firmware", "firmware-sof-signed", "alsa-sof-firmware")
     install_package("linux-firmware", "linux-firmware", "linux-firmware")
-    src = Path("configs/sof.conf")
-    dst = Path("/etc/modprobe.d/sof.conf")
-    dst.write_bytes(src.read_bytes())
+    install_package("pulseaudio", "pulseaudio", "pulseaudio")
+
+
+    # Copy config files
+    cpfile("configs/sof/audio-reload.service", "/etc/systemd/system/alsa-reload.service")
+    cpfile("configs/sof/alsa-sof.conf", "/etc/modprobe.d/alsa-sof.conf")
+    cpfile("configs/sof/asound.conf", "/etc/asound.conf")
+    cpfile("configs/sof/default.pa", "/etc/pulse/default.pa")
+    cpfile("configs/sof/pulseaudio.service", "/usr/lib/systemd/user/pulseaudio.service")
+
+    # Extra commands
+    subprocess.run("systemctl daemon-reload")
+    subprocess.run("systemctl enable alsa-reload")
+
+    # These commands need to be ran as a regular user
+    subprocess.run("su -c 'systemctl --user mask pipewire.{socket,service}' " + username)
+    subprocess.run("su -c 'systemctl --user unmask pulseaudio.{socket,service}' " + username)
+    subprocess.run("su -c 'systemctl --user enable pulseaudio.{socket,service}' " + username)
 
 def str_audio():
     print("str audio not implemented yet")
@@ -33,7 +54,6 @@ def detect_platform():
         exit(1)
 
 def install_package(arch_package, deb_package, rpm_package):
-    pkgmgmt = ""
     if Path("/usr/bin/pacman").exists():
         os.system(f"pacman -S --noconfirm {arch_package}")
     elif Path("/usr/bin/apt").exists():
@@ -45,6 +65,9 @@ def install_package(arch_package, deb_package, rpm_package):
         exit(1)
 
 if __name__ == "__main__":
+    # Get the username of the user running the script before elevating to root
+    username = os.getlogin()
+
     # Elevate script to root
     if not os.geteuid() == 0:
         sudo_args = ['sudo', sys.executable] + sys.argv + [os.environ]
@@ -63,15 +86,15 @@ if __name__ == "__main__":
             case "apl":
                 apl_audio()
             case "glk":
-                sof_audio()
+                sof_audio("glk", username)
             case "whl":
-                sof_audio()
+                sof_audio("whl", username)
             case "cml":
-                sof_audio()
+                sof_audio("cml", username)
             case "jsl":
-                sof_audio()
+                sof_audio("jsl", username)
             case "tgl":
-                sof_audio()
+                sof_audio("tgl", username)
             case "str":
                 str_audio()
             case "zen2":
